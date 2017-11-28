1a12587a17f5adc7a6b18cff25e7708c82963dcc
Austin Pursley <austin.t.pursley@gmail.com>
2017-11-28 11:33:07 -0600

    skin temperature, finalized progress

--- heat/txt_to_mat.h
+++ heat/txt_to_mat.h
@@ -7,23 +7,23 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "stdafx.h"
 
-cv::Mat txt_to_mat(std::string txt_file, int height, int width) {
-	
+cv::Mat txt_to_mat(std::string txt_file, int width, int height) {
+	//The data in txt will be integer values, hence CV_32SC1
 	cv::Mat mat(height, width, CV_32SC1);
 	std::ifstream ifs(txt_file);
-	std::string tempstr;
+	std::string row;
 	int tempint;
-	int row = 0;
-	int column = 0;
-	while (std::getline(ifs, tempstr)) {
-		column = 0;
-		std::istringstream iss(tempstr);
+	int i = 0;
+	int j = 0;
+	while (std::getline(ifs, row)) {
+		j = 0;
+		std::istringstream iss(row);
 		std::vector<int> tempv;
 		while (iss >> tempint) {
-			mat.at<int>(row, column) = tempint;
-			column++;
+			mat.at<int>(i, j) = tempint;
+			j++;
 		}
-		row++;
+		i++;
 	}
 	return mat;
 }
