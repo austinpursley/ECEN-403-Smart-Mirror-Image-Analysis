136f292769f074be32fa42c6e6525b90e0cd9e71
Austin Pursley <austin.t.pursley@gmail.com>
2017-12-29 02:08:36 -0600

    lesion detection/localization color space experimenting

--- lesions/lesion_detection.h
+++ lesions/lesion_detection.h
@@ -1,17 +1,20 @@
+#include "stdafx.h"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "lesions.h"
-#include "stdafx.h"
 #include <time.h>
 
 std::vector<std::vector<cv::Point>> lesion_detection(const cv::Mat & image, std::string img_name) {
 	///VARIABLES / SETTINGS
 	//all tuning/performance parameters in one place.
-	int gauss_ksize = 25;
-	int blocksize = 25;
-	int size_close = 3;
-	int size_open = 3;
-	int size_dilate = 0;
+	int gauss_ksize = 19;
+	int blocksize = 45;
+	int size_close = 2;
+	int size_open = 2;
+
+	int size_close2 = 4;
+	int size_open2 = 2;
+	
 	//guassian blur
 	cv::Size ksize;
 	ksize.height = gauss_ksize;
@@ -31,13 +34,17 @@ std::vector<std::vector<cv::Point>> lesion_detection(const cv::Mat & image, std:
 	cv::Mat elem_open = cv::getStructuringElement(shape,
 		cv::Size(2 * size_open + 1, 2 * size_open + 1),
 		cv::Point(size_open, size_open));
-	cv::Mat elem_dilate = cv::getStructuringElement(shape,
-		cv::Size(2 * size_dilate + 1, 2 * size_dilate + 1),
-		cv::Point(size_dilate, size_dilate));
+
+	cv::Mat elem_open2 = cv::getStructuringElement(shape,
+		cv::Size(2 * size_open2 + 1, 2 * size_open2 + 1),
+		cv::Point(size_open2, size_open2));
+	cv::Mat elem_close2 = cv::getStructuringElement(shape,
+		cv::Size(2 * size_close2 + 1, 2 * size_close2 + 1),
+		cv::Point(size_close2, size_close2));
 	//countour
 	std::vector<std::vector<cv::Point>> contours;
 	//matrices for each step of process
-	cv::Mat gr_img, blur_img, gray_img, thresh_img, close_img, open_img, dilate_img;
+	cv::Mat gr_img, blur_img, gray_img, thresh_img, close_img, open_img;
 	
 	///PROCESS
 	/*
@@ -58,8 +65,7 @@ std::vector<std::vector<cv::Point>> lesion_detection(const cv::Mat & image, std:
 	cv::adaptiveThreshold(gray_img, thresh_img, maxValue, adaptMethod, thresholdType, blocksize, 2);  
 	cv::morphologyEx(thresh_img, close_img, close, elem_close);
 	cv::morphologyEx(close_img, open_img, open, elem_open); 
-	cv::dilate(open_img, dilate_img, elem_dilate); 
-	cv::findContours(dilate_img, contours, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+	cv::findContours(open_img, contours, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
 
 
 	///------------- TESTING / DEBUG ---------------------
@@ -72,19 +78,51 @@ std::vector<std::vector<cv::Point>> lesion_detection(const cv::Mat & image, std:
 	std::string out_file = img_out_dir + "/num_of_lesions.txt";
 	file = fopen(out_file.c_str(), "w");
 
-	cv::bitwise_not(dilate_img, dilate_img);
+	///COLOR SPACE EXPERIMENTS
+
+	cv::Mat lab_img, ab_img, cr_img, yrb_img;
+	cv::GaussianBlur(gr_img, blur_img, ksize, 0);
+	cv::cvtColor(blur_img, lab_img, CV_BGR2Lab, 3);
+	cv::cvtColor(blur_img, yrb_img, CV_BGR2YCrCb, 3);
+	std::vector<cv::Mat> lab(3);
+	std::vector<cv::Mat> yrb(3);
+	cv::split(lab_img, lab);
+	cv::split(yrb_img, yrb);
+	
+	lab[0] = yrb[1];
+	cv::merge(lab, ab_img);
+	//cv::cvtColor(ab_img, ab_img, CV_Lab2BGR, 3);
+	cv::cvtColor(ab_img, gray_img, CV_BGR2GRAY, 1);
+	
+	//gray_img = lab[1];
+	cv::bitwise_not(gray_img, gray_img);
+	cv::adaptiveThreshold(gray_img, thresh_img, maxValue, adaptMethod, thresholdType, blocksize, 2);
+	//cv::bitwise_not(thresh_img, thresh_img);
+	//cv::morphologyEx(thresh_img, thresh_img, open, elem_open);
+	cv::morphologyEx(thresh_img, close_img, close, elem_close);
+	cv::morphologyEx(close_img, open_img, open, elem_open);
+	cv::morphologyEx(open_img, close_img, close, elem_close2);
+	cv::morphologyEx(close_img, open_img, open, elem_open2);
+	cv::findContours(open_img, contours, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+	///---------------------
+	
+	cv::bitwise_not(open_img, open_img);
 	cv::Mat masked, color;
-	cv::cvtColor(dilate_img, color, CV_GRAY2BGR);
+	cv::cvtColor(open_img, color, CV_GRAY2BGR);
 	cv::bitwise_and(color, image, masked);
 
+	
 	cv::imwrite(img_out_dir + "_0_bgr.jpg", image);
+	/*
 	cv::imwrite(img_out_dir + "_1_gr_" + img_name + ".jpg", gr_img);
 	cv::imwrite(img_out_dir + "_2_blur_" + img_name + ".jpg", blur_img);
-	cv::imwrite(img_out_dir + "_3_gray_" + img_name + ".jpg",  gray_img);
+	*/
+	cv::imwrite(img_out_dir + "_3_gray_" + img_name + ".jpg", gray_img);
+	cv::imwrite(img_out_dir + "_2_lab_" + img_name + ".jpg", lab_img);
+	//cv::imwrite(img_out_dir + "_3_ab_" + img_name + ".jpg", ab_img);
 	cv::imwrite(img_out_dir + "_4_thresh_" + img_name + ".jpg", thresh_img);
 	cv::imwrite(img_out_dir + "_5_close_" + img_name + ".jpg",  close_img);
 	cv::imwrite(img_out_dir + "_6_open_" + img_name + ".jpg",   open_img);
-	cv::imwrite(img_out_dir + "_7_dilate_" + img_name + ".jpg", dilate_img);
 	cv::imwrite(img_out_dir + "_8_masked_" + img_name + ".jpg", masked);
 	
 	int num_lesions = contours.size() - 1;
--- lesions/lesion_entropy_detect.h
+++ lesions/lesion_entropy_detect.h
@@ -1,7 +1,7 @@
+#include "stdafx.h"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "lesions.h"
-#include "stdafx.h"
 #include <time.h>
 #include <iostream>
 
